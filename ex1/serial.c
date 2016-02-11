7090fe17c7ca5e1f8c041669a2a181f6ec7410dd
jvals <jvals@users.noreply.github.com>
2016-02-11 20:44:06 +0100

    Fix some indentation inconsistencies.

--- ex1/serial.c
+++ ex1/serial.c
@@ -12,7 +12,7 @@ int main() {
     // Vector to hold the partial sums
     double v[MAX];
     for(uint16_t i = 1; i <= MAX; i++) {
-	v[i] = 1 / (double)(i * i);
+        v[i] = 1 / (double)(i * i);
     }
 
     // ----- Compute sum S(n)
@@ -25,13 +25,13 @@ int main() {
     // New vector containing the errors
     double e[11];
     for(uint8_t k = 3; k <= 14; k++) {
-	double partial_sum = compute_sum(v, pow(2,k));
-	e[k-3] = fabs(wolfram_sum - partial_sum);
+        double partial_sum = compute_sum(v, pow(2,k));
+        e[k-3] = fabs(wolfram_sum - partial_sum);
     }
 
     // ----- Print out the error |S - S(n)|
     for(uint8_t i = 0; i <= 11; i++) {
-	printf("k = %d,\terror = %f\n", i+3, e[i]);
+        printf("k = %d,\terror = %f\n", i+3, e[i]);
     }
 
     return 0;
@@ -40,7 +40,7 @@ int main() {
 double compute_sum(double v[], uint16_t n) {
     double sum = 0.0;
     for(uint16_t i = 1; i <= n; i++) {
-	sum += v[i];
+        sum += v[i];
     }
     return sum;
 }
