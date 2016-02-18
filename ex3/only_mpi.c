57cbc12d8a0006bac8856b9e3ab48779a8648551
jvals <jvals@users.noreply.github.com>
2016-02-18 18:18:34 +0100

    Start implementation of mpi version

---
+++ ex3/only_mpi.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+// Prototypes
+double compute_sum(double v[], uint16_t);
+
+int main(int argc, char *argv[]) {
+    // ----- Get input
+    if(argc < 2) {
+	printf("Usage: ./only_omp.c limit\n limit is an integer\n");
+	return 1;
+    }
+    uint8_t k = atoi(argv[1]);
+    uint16_t n = pow(2, k);
+    
+    // ----- Generate vector v
+    // Vector to hold the partial sums
+    double v[n];
+    for(uint16_t i = 1; i <= n; i++) {
+        v[i] = 1 / (double)(i * i);
+    }
+
+    // ----- Compute sum S(n)
+    double total_sum = compute_sum(v, n);
+    printf("Total Sum: %f\n", total_sum);
+
+    // ----- Compute the error |S - S(n)|
+    // The actual sum (computed by wolfram alpha) with 16 digits of accuracy
+    double wolfram_sum = 1.644934066848226;
+    double error = fabs(wolfram_sum - total_sum);
+
+    // ----- Print out the error |S - S(n)|
+    printf("n = %d,\terror = %f\n", n, error);
+    
+    return 0;
+}
+
+double compute_sum(double v[], uint16_t n) {
+    double sum = 0.0;
+    for(uint16_t i = 1; i <= n; i++) {
+        sum += v[i];
+    }
+    return sum;
+}
