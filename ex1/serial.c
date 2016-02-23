5d13e07e1dfd09b0920c29903f24dbcb6113b618
jvals <jvals@users.noreply.github.com>
2016-02-23 08:28:22 +0100

    Change some tabs to spaces in serial, and change output to 16 decimals.

--- ex1/serial.c
+++ ex1/serial.c
@@ -9,8 +9,8 @@ double compute_sum(double v[], uint16_t);
 int main(int argc, char *argv[]) {
     // ----- Get input
     if(argc < 2) {
-	printf("Usage: ./serial.c limit\n limit is an integer\n");
-	return 1;
+        printf("Usage: ./serial.c limit\n limit is an integer\n");
+        return 1;
     }
     uint8_t k = atoi(argv[1]);
     uint16_t n = pow(2, k);
@@ -24,7 +24,7 @@ int main(int argc, char *argv[]) {
 
     // ----- Compute sum S(n)
     double total_sum = compute_sum(v, n);
-    printf("Total Sum: %f\n", total_sum);
+    printf("Total Sum: %.16f\n", total_sum);
 
     // ----- Compute the error |S - S(n)|
     // The actual sum (computed by wolfram alpha) with 16 digits of accuracy
@@ -32,7 +32,7 @@ int main(int argc, char *argv[]) {
     double error = fabs(wolfram_sum - total_sum);
 
     // ----- Print out the error |S - S(n)|
-    printf("n = %d,\terror = %f\n", n, error);
+    printf("n = %d,\terror = %.16f\n", n, error);
     
     return 0;
 }
