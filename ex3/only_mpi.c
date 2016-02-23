6399637c9f41940ed85b4ea75383b657f8ad9d51
jvals <jvals@users.noreply.github.com>
2016-02-23 08:37:35 +0100

    Finalize the MPI version.

--- ex3/only_mpi.c
+++ ex3/only_mpi.c
@@ -5,13 +5,13 @@
 #include <mpi.h>
 
 // Prototypes
-double compute_sum(double v[], uint16_t);
+double compute_sum(double v[], uint16_t n, int my_rank, int nprocs);
 
 int main(int argc, char *argv[]) {
-    // ----- Get input
-    if(argc < 2) {
-	printf("Usage: ./only_omp.c limit\n limit is an integer\n");
-	return 1;
+    // Get input
+    if (argc < 2) {
+        printf("Usage: ./only_omp.c limit\n limit is an integer\n");
+        return 1;
     }
 
     int nprocs, my_rank;
@@ -23,9 +23,9 @@ int main(int argc, char *argv[]) {
     MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
 
     // Rank 0 does I/O
-    if(my_rank == 0) {
+    if (my_rank == 0) {
         uint8_t k = atoi(argv[1]);
-	n = pow(2, k);
+        n = pow(2, k);
     }
 
     // Every rank needs to know the size of the problem
@@ -33,33 +33,46 @@ int main(int argc, char *argv[]) {
 
     // ----- Generate vector v
     // Vector to hold the partial sums
-    double *v = (double *) malloc(n);
-    if(my_rank == 0) {
-	for(uint16_t i = 1; i <= n; i++) {
-	    v[i] = 1 / (double)(i * i);
-	}
+    double *v = (double *) malloc(n * sizeof(double));
+    if (my_rank == 0) {
+        for (uint16_t i = 1; i <= n; i++) {
+            v[i-1] = 1 / (double)(i * i);
+        }
     }
+    // Broadcast the generated vector to every rank
+    MPI_Bcast(v, n, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
     // ----- Compute sum S(n)
-    double total_sum = compute_sum(v, n);
-    printf("Total Sum: %f\n", total_sum);
+    double total_sum;
+    double partial_sum = compute_sum(v, n, my_rank, nprocs);
+    MPI_Reduce(&partial_sum, &total_sum, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
 
-    // ----- Compute the error |S - S(n)|
-    // The actual sum (computed by wolfram alpha) with 16 digits of accuracy
-    double wolfram_sum = 1.644934066848226;
-    double error = fabs(wolfram_sum - total_sum);
+    if (my_rank == 0) {
+        printf("Total Sum: %.16f\n", total_sum);
 
-    // ----- Print out the error |S - S(n)|
-    printf("n = %d,\terror = %f\n", n, error);
+        // ----- Compute the error |S - S(n)|
+        // The actual sum (computed by wolfram alpha) with 16 digits of accuracy
+        double wolfram_sum = 1.644934066848226;
+        double error = fabs(wolfram_sum - total_sum);
+
+        // ----- Print out the error |S - S(n)|
+        printf("n = %d,\terror = %.16f\n", n, error);
+    }
 
     free(v);
+    MPI_Finalize();
     return 0;
 }
 
-double compute_sum(double v[], uint16_t n) {
+double compute_sum(double v[], uint16_t n, int my_rank, int nprocs) {
     double sum = 0.0;
-    for(uint16_t i = 1; i <= n; i++) {
+
+    uint16_t my_start = (n * my_rank) / nprocs;
+    uint16_t my_stop = (n * (my_rank + 1)) / nprocs;
+
+    for (uint16_t i = my_start; i < my_stop; i++) {
         sum += v[i];
     }
+
     return sum;
 }
